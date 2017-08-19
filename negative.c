a9cefb20e914b352fb3e27741ee4dffa9626bc20
sahayaraj1996 <30927248+sahayaraj1996@users.noreply.github.com>
2017-08-19 17:01:39 +0530

    Rename positive.c to negative.c

---
+++ negative.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+int main()
+{ 
+  int num;
+  printf("Enter a number\n");
+  scanf("%d", &num);
+  if(num>0)
+  {
+   printf("%d is positive number", number);
+   }
+   else if(num<0)
+   {
+   printf("%d is negative number", number);
+   }
+   if
+   {
+   printf("Input number is zero");
+   }
+   return 0;
+   }
